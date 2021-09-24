90a04a86254ebca22246a2a9d48b9f01e64a9fb1
Jordan Graves <Jordang3177@gmail.com>
2021-09-24 17:42:59 -0400

    MachineLearning lesson for today

---
+++ Learning/Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/JG_Multiple_Linear_Regression.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
